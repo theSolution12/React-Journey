8755b6e1b77b452d42ef111d1b0dbc6b8dde00c4
Parth Patidar <parth1207patidar@gmail.com>
2025-04-20 17:12:05 +0530

    Replace Header, MainComponent, and Footer with Greeting and ProductInfo components in App

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Greet from "./components/Greet";
 import MainComponent from "./components/MainComponent";
+import Greeting from "./components/Greeting";
+import ProductInfo from "./components/ProductInfo";
 
 const App = () => {
   return (
     <>
-      <Header />
-      <MainComponent />
-      <Footer />
+      <Greeting />
+      <ProductInfo />
     </>
   )
 }
