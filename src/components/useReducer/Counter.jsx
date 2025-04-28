9f9b8a91245e9e248fb04a7762c8f60598060acf
Parth Patidar <parth1207patidar@gmail.com>
2025-04-28 21:48:36 +0530

    Refactor Counter component to use dynamic payload
    Added state management for payload input in the Counter component. Updated increment and decrement actions to use the payload value instead of a fixed number.

--- src/components/useReducer/Counter.jsx
+++ src/components/useReducer/Counter.jsx
@@ -1,16 +1,19 @@
 import { initialState, reducer } from "./CounterReducer";
-import { useReducer } from "react";
+import { useReducer, useState } from "react";
 
 const Counter = () => {
     const [state, dispatch] = useReducer(reducer, initialState);
+    const [payload, setPayload] = useState(0);
 
     return(
         <div>
             <h1>Count: {state.count}</h1>
+            <label htmlFor="payload">payload</label>
+            <input type="number" id="payload" onChange={(e) => setPayload(Number(e.target.value))} value={payload} />
             <button onClick={() => dispatch({type: 'increment'})}>Increment</button>
             <button onClick={() => dispatch({type: 'decrement'})}>Decrement</button>
-            <button onClick={() => dispatch({type: 'incrementByAmount', payload: 5})}>Increment by 5</button>
-            <button onClick={() => dispatch({type: 'decrementByAmount', payload: 5})}>Decrement by 5</button>
+            <button onClick={() => dispatch({type: 'incrementByAmount', payload: payload})}>Increment by payload</button>
+            <button onClick={() => dispatch({type: 'decrementByAmount', payload: payload})}>Decrement by payload</button>
             <button onClick={() => dispatch({type: 'reset'})}>Reset</button>
         </div>
     )
