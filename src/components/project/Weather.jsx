a7e9c1f71527b0303aacb74c01b9911d7c76d6e6
Parth Patidar <parth1207patidar@gmail.com>
2025-04-30 22:42:32 +0530

    Add Weather component and useDebounce hook
    Implemented Weather component to fetch and display weather data based on user input. Added useDebounce hook for debouncing city input changes.

---
+++ src/components/project/Weather.jsx
@@ -0,0 +1,48 @@
+import { useState, useEffect } from "react";
+import { useDebounce } from "../useDebounce";
+
+const Weather = () => {
+    const key = import.meta.env.VITE_WEATHER_API_KEY;
+
+    const [weather, setWeather] = useState(null);
+    const [city, setCity] = useState('');
+    const debouncedCity = useDebounce(city, 500);
+
+    useEffect(() => {
+        const fetchWeather = async (city) => {
+            try {
+                const baseURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&units=metric`;
+                const response = await fetch(baseURL);
+                if (!response.ok) {
+                    throw new Error('City not found');
+                }
+                const data = await response.json();
+                setWeather(data);
+            } catch (error) {
+                console.error(error.message);
+            }
+        }
+
+        if (debouncedCity) {
+            fetchWeather(debouncedCity);
+        }
+
+    }, [debouncedCity, key])
+
+    return (
+        <div>
+            <h1>Weather App</h1>
+            <input type="text" placeholder="Enter city" value={city} onChange={(e) => setCity(e.target.value)} />
+            {weather && (
+                <div>
+                    <h2>Weather in {weather.name}</h2>
+                    <p>Temperature: {weather.main.temp} K</p>
+                    <p>Weather: {weather.weather[0].description}</p>
+                </div>
+            )}
+        </div>
+    )
+
+}
+
+export default Weather;
